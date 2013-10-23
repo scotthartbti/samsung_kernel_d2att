10ffd1436001e5eba2ef26ac25ce85a4b1bb2730
scott <scott@ubuntu.com>
2013-10-22 23:39:09 -0400

    correct min boot speed Signed-off-by: scott <scott@ubuntu.com>
    Change-Id: I37e2394b3ead03d9caa84a8c57160e0368e5db14

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -326,7 +326,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	init_completion(&cpu_work->complete);
 
 	policy->max = 1512000;
-	policy->min = 192000;
+	policy->min = 384000;
 	
 	return 0;
 }
